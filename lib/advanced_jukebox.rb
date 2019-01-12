c150b1c271d10e7e9ab9c7f9fd9aee58eea01458
Jared Harbison <jared.harbison@gmail.com>
2019-01-12 23:20:06 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -12,17 +12,14 @@
 # "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
-def help
-  help = <<-HELP
-I accept the following commands:
+def help 
+  puts 
+"I accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
-- exit : exits this program
-HELP
-
-  puts help
-end
+- exit : exits this program"
+end 
 
 
 
